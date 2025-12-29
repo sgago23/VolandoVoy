bec61d2de89477b2aaf3b25612ff486bfd26fa84
sgago23 <s.gago2003@gmail.com>
2025-12-29 13:51:50 +0100

    Estructura inicial proyecto

---
+++ src/main/java/com/example/VolandoVoy/VolandoVoyApplication.java
@@ -0,0 +1,13 @@
+package com.example.VolandoVoy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolandoVoyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VolandoVoyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/VolandoVoy/VolandoVoyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.VolandoVoy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VolandoVoyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
